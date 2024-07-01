3f5a219f12f65aeb30a22488a34933ffab4eafba
Drill <drillelectro@gmail.com>
2024-07-01 22:00:09 +0300

    lesson4-5

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 //document.write("Привет");
-        // console.log("Привет");//в консоль
-        // console.info("Привет");
-        // console.error("Привет");
-        console.warn("Привет!");
+// console.log("Привет");//в консоль
+// console.info("Привет");
+// console.error("Привет");
+console.warn("Привет!");
+
+
+const num = 5;
+
+console.log("Переменная " + num);
+
+var number;
+number = false;
+
+var num_1 = 5;
+var num_2 = "5";
+
+console.log(num_1 + num_2);
+
+var str_1 = Number("12");
+var str_2 = Number("2");
+
+console.log("Результат " + (str_1 + str_2));
+
+console.log("Math: " + Math.PI);
+console.log("Math: " + Math.min(3, 5, 0));
