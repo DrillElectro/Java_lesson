4902487ac5bc7f3733462faf5faa09fe9cad89dd
Drill <drillelectro@gmail.com>
2024-07-09 20:53:15 +0300

    lesson7

--- js/main.js
+++ js/main.js
@@ -2,25 +2,34 @@
 // console.log("Привет");//в консоль
 // console.info("Привет");
 // console.error("Привет");
-console.warn("Привет!");
+// console.warn("Привет!");
 
 
-const num = 5;
+// const num = 5;
 
-console.log("Переменная " + num);
+// console.log("Переменная " + num);
 
-var number;
-number = false;
+// var number;
+// number = false;
 
-var num_1 = 5;
-var num_2 = "5";
+// var num_1 = 5;
+// var num_2 = "5";
 
-console.log(num_1 + num_2);
+// console.log(num_1 + num_2);
 
-var str_1 = Number("12");
-var str_2 = Number("2");
+// var str_1 = Number("12");
+// var str_2 = Number("2");
 
-console.log("Результат " + (str_1 + str_2));
+// console.log("Результат " + (str_1 + str_2));
 
-console.log("Math: " + Math.PI);
-console.log("Math: " + Math.min(3, 5, 0));
+// console.log("Math: " + Math.PI);
+// console.log("Math: " + Math.min(3, 5, 0));
+
+
+//массивы
+var arr = [2, 11, "tr", -7, 0.4];
+console.log(arr.length);
+
+var matrix = [[0,2], ["stroka", 0.5], [-4, 555]];
+matrix[1][0] = 90;
+console.log(matrix);
